940b4ac7500ac04ebf3b774843a03f187bf57428
Yanxi Liu <k1908598@gmail.com>
2024-12-23 01:10:29 -0600

    Update index.html and script.js

--- Desktop/FriedEggFamilyOS/script.js
+++ Desktop/FriedEggFamilyOS/script.js
@@ -2,25 +2,61 @@ let initialX = 0;
 let initialY = 0;
 let currentX = 0;
 let currentY = 0;
+let draggedElement;
 
 
-dragElement(document.getElementById("window"));
+var welcomeScreen;
+var welcomeScreenClose;
+var welcomeScreenOpen;//Chat says that even if i declared those globally, if I am trying to use these variables before they are assigned (outside window.onload), they will still be undefined
 
 
-function dragElement(element){
+window.onload = () => {
+    updateTime();
+    setInterval(updateTime, 1000);
 
+    dragElement(document.getElementById("window"));
 
-    if (document.getElementById(element.id + "header")){
-        document.getElementById(element.id + "header").onmousedown = startDragging;
+    var welcomeScreen = document.querySelector("#welcome");
+    var welcomeScreenClose = document.querySelector("#welcomeclose");
+    var welcomeScreenOpen = document.querySelector("#welcomeopen");
+
+    if (welcomeScreenClose && welcomeScreen) {
+        welcomeScreenClose.addEventListener("click", function () {
+            closeWindow(welcomeScreen);
+        })
     }
-    else{
-        element.onmousedown = startDragging;
+
+    if (welcomeScreenOpen && welcomeScreen) {
+
+        welcomeScreenOpen.addEventListener("click", function () {
+            openWindow(welcomeScreen);
+        })
+    }
+
+}
+
+
+function dragElement(element) {
+
+
+    if (document.getElementById(element.id + "header")) {
+        document.getElementById(element.id + "header").onmousedown = function(e){
+            startDragging(e, element);
+        };
+    }
+    else {
+        //attach the mousedown listener to the element itself
+        element.onmousedown = function(e){
+            startDragging(e, element);
+        }
     }
 }
 
-function startDragging(e){
+function startDragging(e, element) {
     e = e || window.event;
     e.preventDefault();
+
+    draggedElement = element;
 
     initialX = e.clientX;
     initialY = e.clientY;
@@ -29,7 +65,7 @@ function startDragging(e){
     document.onmousedown = dragHandler;
 }
 
-function dragHandler(e){
+function dragHandler(e) {
     e = e || window.event;
     e.preventDefault();
 
@@ -38,22 +74,29 @@ function dragHandler(e){
     initialX = e.clientX;
     initialY = e.clientY;
 
-    element.style.top = (element.offsetTop - currentY)+"px";
-    element.style.left = (element.offsetLeft - currentX)+"px";
+    draggedElement.style.top = (draggedElement.offsetTop - currentY) + "px";
+    draggedElement.style.left = (draggedElement.offsetLeft - currentX) + "px";
 }
 
 
-function stopDragging(){
+function stopDragging() {
     document.onmouseup = null;
     document.onmousemove = null;
+
+    draggedElement = null;
 }
+
+function closeWindow(element) {
+    element.style.display = "none";
+}
+
+function openWindow(element) {
+    element.style.display = "flex";
+}
+
 function updateTime() {
     let timeText = document.querySelector("#timeElement");
     timeText.innerHTML = new Date().toLocaleString();
 }
 
-window.onload=()=>{
-    updateTime();
-    setInterval(updateTime, 1000);
-}
 
